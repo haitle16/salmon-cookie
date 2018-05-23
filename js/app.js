3344ab114bd3422b51791ca4314d7e78de646d00
Hai Le <haile@Hais-MBP.localdomain>
2018-05-22 19:18:34 -0700

    totalCookiesadded

--- js/app.js
+++ js/app.js
@@ -1,24 +1,50 @@
 'use strict'
  
-var storeHours= ['06:00','07:00','08:00','09:00','10:00','11:00','12:00','1:00','2:00','3:00','4:00','5:00','6:00','7:00','8:00'];
+var storeHours= ['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM'];
 
 var pikeStore = {
-    min : 3,
-    max : 8,
-    // this is the number of cooklies average
-    average: 6.1,
-    randomCustomer: function(min, max){
-        min = this.min;
-        max = this.max;
-        // use this.min this.max
-        return Math.random() * (max-min) + min;
+    storeName: 'First and Pike',
+    minCustHourly: 23,
+    maxCustHourly: 65,
+    avgCookiesSoldPerCust: 6.3,
+    randomCustHourly: [],
+    cookiesSoldHourly: [],
+    totalCookies: 0,
+    calcRandomCustHourly: function(){
+        for(var i =0; i < storeHours.length; i++){
+            this.randomCustHourly.push(Math.floor(Math.random() * (this.maxCustHourly - this.minCustHourly+1))+this.minCustHourly);
+            console.log(this.randomCustHourly[i]);
+        }
+    },
+    calcCookiesSoldHourly: function(){
+        for(var j = 0; j < storeHours.length; j++){
+            this.cookiesSoldHourly.push(Math.round(this.avgCookiesSoldPerCust * this.randomCustHourly[j]));
+            console.log(this.cookiesSoldHourly);
+        }
+    },
+    calcTotalDailyCookiesSold: function(){
+        for(var k = 0; k < storeHours.length; k++){
+            this.totalCookies = this.totalCookies + this.cookiesSoldHourly[k];
+        }
+        return this.totalCookies;
+    },
+    render: function(){
+        this.calcRandomCustHourly();
+        this.calcCookiesSoldHourly();
+        this.calcTotalDailyCookiesSold();
+        var hourlyCookiesSold = document.getElementById('hourlyCookiesSold'); // ul list
+        var storeName = document.getElementById('storeName'); // h2
+        var h2Element = document.createElement('h2');
+        h2Element.textContent = this.storeName;
+        storeName.appendChild(h2Element);
+        for(var l = 0; l < storeHours.length; l++){
+            var liElement = document.createElement('li');
+            liElement.textContent = storeHours[l]+ ': '+ this.cookiesSoldHourly[l]+ ' cookies';
+            console.log(liElement);
+            hourlyCookiesSold.appendChild(liElement);
+        }
+        console.log(this.totalCookies);
+        document.write('Total Cookies Sold Today: '+ this.totalCookies);
     }
-    
 }
-// make a for loop that runs for 15 times (hours) for the random custumer generate 
-// put them in an array 
-//build a table for your html google
-
-pikePlace.getRandom(pikePlace.min, pikePlace.max);
- 
-// property of a object that is a function is a METHOD!
+pikeStore.render();
